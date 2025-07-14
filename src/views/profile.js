010e8f7f7192e7df509d0c02733b3a18e12066bf
Tray <tracymedina.dev@gmail.com>
2025-07-14 23:32:59 +0100

    added router and navbar

---
+++ src/views/profile.js
@@ -0,0 +1,6 @@
+import "../assets/styles/profile.css";
+
+export default function profile(container) {
+  //Dentro de esta función iría todo el contenido de profile
+  container.innerHTML = "This is the profile page";
+}
---
+++ src/views/workshops.js
@@ -0,0 +1,6 @@
+import "../assets/styles/workshops.css";
+
+export default function workshops(container) {
+  //Dentro de esta función iría todo el contenido de workshops
+  container.innerHTML = "This is the My workshops page";
+}
