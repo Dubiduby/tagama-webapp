d242cf4aed6eb44e19d378651f56d4b5fb63a37b
Yesenia <yeseniamedina.dev@gmail.com>
2025-07-15 11:36:56 +0100

    Added validations,spinner and spinner.css

--- src/views/login.js
+++ src/views/login.js
@@ -2,5 +2,6 @@ import "../assets/styles/login.css";
 
 export default function login(container) {
   //Dentro de esta función iría todo el contenido de login
-  container.innerHTML = "This is the login page";
+  container.innerHTML = "This is the login page"
+
 }
