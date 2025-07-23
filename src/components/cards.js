384e47b3ffa39cf13676f4bddc545b2f28e987c2
Tray <tracymedina.dev@gmail.com>
2025-07-23 14:34:09 +0100

    fixed reapeated code

--- src/components/cards.js
+++ src/components/cards.js
@@ -52,7 +52,7 @@ export function workshopCards(workshop, subcategory, category) {
   dateSpan.className = "date";
   dateSpan.innerHTML = `<img src="${
     new URL("../assets/images/calendar.svg", import.meta.url).href
-  }" alt="Calendar">  ${workshop.date}`;
+  }" alt="Calendar">  ${dayjs.unix(workshop.date).format("DD/MM/YYYY")}`;
   //dateSpan.textContent= workshop.date;
 
   const locationSpan = document.createElement("span");
@@ -74,7 +74,8 @@ export function workshopCards(workshop, subcategory, category) {
   }" alt="Spots">  ${workshop.enrolled.length}/${workshop.capacity}`;
 
   const workshopPrice = document.createElement("p");
-  workshopPrice.textContent = `${workshop.price}€`;
+  workshopPrice.textContent =
+    workshop.price === 0 ? "Free" : `${workshopPrice}`;
 
   const workshopTags = document.createElement("span");
   workshopTags.className = "tags";
@@ -101,7 +102,6 @@ export function workshopCards(workshop, subcategory, category) {
   buttonAdd.addEventListener("click", (event) => {
     event.preventDefault();
     event.stopPropagation();
-
     const currentUser = getCurrentUser();
     // Verifica si ya está guardado y en que posición se encuentra:
     const position = currentUser.savedWorkshops.indexOf(workshop.id);
@@ -132,88 +132,4 @@ export function workshopCards(workshop, subcategory, category) {
   cardLink.appendChild(card);
 
   return cardLink;
-}
-    
-
-    const divCardInfo= document.createElement("div");
-    divCardInfo.className = "card-info";
-    const titleCard = document.createElement("h3");
-    titleCard.textContent = workshop.title;
-    // divCardInfo.appendChild(titleCard);
-
-    const divCardDetails=document.createElement("div");
-    divCardDetails.className="card-details";
-
-    const dateSpan = document.createElement("span");
-    dateSpan.className = "date";
-    dateSpan.innerHTML =`<img src="${new URL("../assets/images/calendar.svg", import.meta.url).href}" alt="Calendar">  ${dayjs.unix(workshop.date).format("DD/MM/YYYY")}`;
-    //dateSpan.textContent= workshop.date;
-
-    const locationSpan = document.createElement("span");
-    locationSpan.className = "location";
-    locationSpan.innerHTML =`<img src="${new URL("../assets/images/location-pin.svg", import.meta.url).href}" alt="Location">  ${workshop.mode}`;
-
-    const durationSpan = document.createElement("span");
-    durationSpan.className = "duration";
-    durationSpan.innerHTML =`<img src="${new URL("../assets/images/time.svg", import.meta.url).href}" alt="Duration">  ${workshop.duration} min`;
-
-    const spots = document.createElement("span");
-    spots.className = "spots";
-    spots.innerHTML =`<img src="${new URL("../assets/images/spots.svg", import.meta.url).href}" alt="Spots">  ${workshop.enrolled.length}/${workshop.capacity}`;
-
-    const workshopPrice = document.createElement("p");
-    workshopPrice.textContent = (workshop.price === 0)? "Free" : `${workshopPrice}`;
-
-    const workshopTags = document.createElement("span");
-    workshopTags.className = "tags";
-    workshopTags.textContent = `${subcategory?.name || ""}/ ${category?.name || ""}`;
-
-    card.appendChild(divCardImage);
-    divCardImage.appendChild(img);
-    divCardImage.appendChild(buttonAdd);
-
-    card.appendChild(divCardInfo);
-    divCardInfo.appendChild(titleCard);
-    divCardInfo.appendChild(divCardDetails);
-
-    divCardDetails.appendChild(dateSpan);
-    divCardDetails.appendChild(locationSpan);
-    divCardDetails.appendChild(durationSpan);
-    divCardDetails.appendChild(spots);
-    divCardDetails.appendChild(workshopPrice);
-    divCardDetails.appendChild(workshopTags);
-
-
-    //TODO:Add event listener icon saved
-    buttonAdd.addEventListener("click", (event)=>{
-        event.preventDefault();
-       event.stopPropagation();
-        const currentUser = getCurrentUser();
-        // Verifica si ya está guardado y en que posición se encuentra:
-  const position= currentUser.savedWorkshops.indexOf(workshop.id);
-        if(position === -1){
-            // No está guardado, entonces lo añadimos
-            currentUser.savedWorkshops.push(workshop.id);
-            buttonAdd.innerHTML = `<img src="${new URL("../assets/images/bookmark-check.svg", import.meta.url).href}" alt="Added to the list">`;
-            buttonAdd.setAttribute("data-workshop-id", workshop.id);
-        }else{
-            // Ya está guardado, entonces lo eliminamos
-            currentUser.savedWorkshops.splice(position, 1);
-            buttonAdd.innerHTML = `<img src="${new URL("../assets/images/bookmark_Plus.svg", import.meta.url).href}" alt="Add to the list">`;
-            buttonAdd.setAttribute("data-workshop-id", workshop.id);  
-        }
-        localStorage.setItem("currentUser", JSON.stringify(currentUser));
-
-        updateUser(currentUser);
-    });
-  
-    //added so be able to go to the detail page
-    const cardLink = document.createElement("a");
-    cardLink.href = `/workshops/${workshop.id}`;
-    cardLink.setAttribute("data-link", "");
-    cardLink.appendChild(card);
-
-    return cardLink;
-
-
 }
